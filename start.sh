7970ed616b3b12bc5cba2abcc1a72ca31f93826a
Allen K <allen@nhpcorp.com>
2026-05-17 19:17:49 -0400

    fix: copy wallet files to bittensor directory on startup

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 pip uninstall scalecodec cyscale -y
 pip install cyscale --force-reinstall
+mkdir -p /root/.bittensor/wallets
+cp -r /app/wallet_backup /root/.bittensor/wallets/bagbot
 python3 bagbot.py --nocheck
