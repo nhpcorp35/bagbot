f542b6924a24554e361f1791b9089d33d73eff95
nhpcorp35 <allenk@jameinc.com>
2026-05-22 17:14:56 -0400

    taonow_sync: auto-update to SN[4, 9, 100, 110]

--- bagbot_settings_overrides.py
+++ bagbot_settings_overrides.py
@@ -1,27 +1,27 @@
 import os
 WALLET_PW = os.environ.get("WALLET_PW", "")
 WALLET_NAME = "bagbot"
-# Auto-updated by taonow_sync at 2026-05-22 21:12 UTC
+# Auto-updated by taonow_sync at 2026-05-22 21:14 UTC
 SUBNET_SETTINGS = {
     4: {
         'max_alpha': 30,
         'max_tao_per_buy': 0.1,
         'max_tao_per_sell': 0.1,
         'max_slippage_percent_per_buy': 1.0,
-        'buy_lower': 0.03362852,
-        'buy_upper': 0.05716848,
-        'sell_lower': 0.07846654,
-        'sell_upper': 0.14011883,
+        'buy_lower': 0.03362859,
+        'buy_upper': 0.0571686,
+        'sell_lower': 0.0784667,
+        'sell_upper': 0.14011911,
     },
     9: {
         'max_alpha': 30,
         'max_tao_per_buy': 0.1,
         'max_tao_per_sell': 0.1,
         'max_slippage_percent_per_buy': 1.0,
-        'buy_lower': 0.01446022,
-        'buy_upper': 0.02458238,
-        'sell_lower': 0.03374052,
-        'sell_upper': 0.06025092,
+        'buy_lower': 0.01446158,
+        'buy_upper': 0.02458469,
+        'sell_lower': 0.03374369,
+        'sell_upper': 0.06025659,
     },
     100: {
         'buy_lower': 0.00649103,
@@ -38,9 +38,9 @@ SUBNET_SETTINGS = {
         'max_tao_per_buy': 0.1,
         'max_tao_per_sell': 0.1,
         'max_slippage_percent_per_buy': 1.0,
-        'buy_lower': 0.00502374,
-        'buy_upper': 0.00854035,
-        'sell_lower': 0.01172205,
-        'sell_upper': 0.02093224,
+        'buy_lower': 0.00496696,
+        'buy_upper': 0.00844383,
+        'sell_lower': 0.01158957,
+        'sell_upper': 0.02069565,
     },
 }
