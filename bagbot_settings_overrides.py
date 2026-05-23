98d41c8746119a557efeecdce84c2c3f566efe11
nhpcorp35 <allenk@jameinc.com>
2026-05-23 03:52:41 -0400

    taonow_sync: auto-update to SN[4, 9, 100, 110, 120]

--- bagbot_settings_overrides.py
+++ bagbot_settings_overrides.py
@@ -1,27 +1,27 @@
 import os
 WALLET_PW = os.environ.get("WALLET_PW", "")
 WALLET_NAME = "bagbot"
-# Auto-updated by taonow_sync at 2026-05-23 07:51 UTC
+# Auto-updated by taonow_sync at 2026-05-23 07:52 UTC
 SUBNET_SETTINGS = {
     4: {
         'max_alpha': 30,
         'max_tao_per_buy': 0.1,
         'max_tao_per_sell': 0.1,
         'max_slippage_percent_per_buy': 1.0,
-        'buy_lower': 0.03351698,
-        'buy_upper': 0.05697887,
-        'sell_lower': 0.07820629,
-        'sell_upper': 0.13965409,
+        'buy_lower': 0.03350944,
+        'buy_upper': 0.05696605,
+        'sell_lower': 0.0781887,
+        'sell_upper': 0.13962267,
     },
     9: {
         'max_alpha': 30,
         'max_tao_per_buy': 0.1,
         'max_tao_per_sell': 0.1,
         'max_slippage_percent_per_buy': 1.0,
-        'buy_lower': 0.01518454,
-        'buy_upper': 0.02581372,
-        'sell_lower': 0.03543059,
-        'sell_upper': 0.06326891,
+        'buy_lower': 0.01520542,
+        'buy_upper': 0.02584922,
+        'sell_lower': 0.03547932,
+        'sell_upper': 0.06335592,
     },
     100: {
         'buy_lower': 0.00649103,
@@ -34,13 +34,23 @@ SUBNET_SETTINGS = {
         'max_slippage_percent_per_buy': 1.0,
     },
     110: {
+        'buy_lower': 0.00418175,
+        'buy_upper': 0.0,
+        'sell_lower': 0.00794533,
+        'sell_upper': 0.01254526,
+        'max_alpha': 1,
+        'max_tao_per_buy': 0.0,
+        'max_tao_per_sell': 0.1,
+        'max_slippage_percent_per_buy': 1.0,
+    },
+    120: {
         'max_alpha': 30,
         'max_tao_per_buy': 0.1,
         'max_tao_per_sell': 0.1,
         'max_slippage_percent_per_buy': 1.0,
-        'buy_lower': 0.00501232,
-        'buy_upper': 0.00852094,
-        'sell_lower': 0.0116954,
-        'sell_upper': 0.02088465,
+        'buy_lower': 0.03748184,
+        'buy_upper': 0.06371912,
+        'sell_lower': 0.08745762,
+        'sell_upper': 0.15617432,
     },
 }
