bee6e29d314ca85c4b4a6ff31f9eb3180f30e47c
nhpcorp35 <allenk@jameinc.com>
2026-05-23 07:38:15 -0400

    taonow_sync: auto-update to SN[4, 9, 100, 120]

--- bagbot_settings_overrides.py
+++ bagbot_settings_overrides.py
@@ -1,27 +1,27 @@
 import os
 WALLET_PW = os.environ.get("WALLET_PW", "")
 WALLET_NAME = "bagbot"
-# Auto-updated by taonow_sync at 2026-05-23 11:36 UTC
+# Auto-updated by taonow_sync at 2026-05-23 11:38 UTC
 SUBNET_SETTINGS = {
     4: {
         'max_alpha': 30,
         'max_tao_per_buy': 0.1,
         'max_tao_per_sell': 0.1,
         'max_slippage_percent_per_buy': 1.0,
-        'buy_lower': 0.03347487,
-        'buy_upper': 0.05690728,
-        'sell_lower': 0.07810803,
-        'sell_upper': 0.13947863,
+        'buy_lower': 0.03347088,
+        'buy_upper': 0.05690049,
+        'sell_lower': 0.07809871,
+        'sell_upper': 0.13946199,
     },
     9: {
         'max_alpha': 30,
         'max_tao_per_buy': 0.1,
         'max_tao_per_sell': 0.1,
         'max_slippage_percent_per_buy': 1.0,
-        'buy_lower': 0.01532223,
-        'buy_upper': 0.0260478,
-        'sell_lower': 0.03575188,
-        'sell_upper': 0.06384263,
+        'buy_lower': 0.01530574,
+        'buy_upper': 0.02601975,
+        'sell_lower': 0.03571339,
+        'sell_upper': 0.06377391,
     },
     100: {
         'buy_lower': 0.00649103,
@@ -33,24 +33,14 @@ SUBNET_SETTINGS = {
         'max_tao_per_sell': 0.1,
         'max_slippage_percent_per_buy': 1.0,
     },
-    110: {
+    120: {
         'max_alpha': 30,
         'max_tao_per_buy': 0.1,
         'max_tao_per_sell': 0.1,
         'max_slippage_percent_per_buy': 1.0,
-        'buy_lower': 0.0050246,
-        'buy_upper': 0.00854183,
-        'sell_lower': 0.01172408,
-        'sell_upper': 0.02093585,
-    },
-    120: {
-        'buy_lower': 0.03145765,
-        'buy_upper': 0.0,
-        'sell_lower': 0.05976953,
-        'sell_upper': 0.09437295,
-        'max_alpha': 1,
-        'max_tao_per_buy': 0.0,
-        'max_tao_per_sell': 0.1,
-        'max_slippage_percent_per_buy': 1.0,
+        'buy_lower': 0.03775089,
+        'buy_upper': 0.06417651,
+        'sell_lower': 0.08808541,
+        'sell_upper': 0.15729537,
     },
 }
