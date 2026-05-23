cd0c17a7099016c64a7e74180d70ce94a3a3bc48
nhpcorp35 <allenk@jameinc.com>
2026-05-23 06:34:12 -0400

    taonow_sync: auto-update to SN[4, 9, 100, 120]

--- bagbot_settings_overrides.py
+++ bagbot_settings_overrides.py
@@ -1,27 +1,27 @@
 import os
 WALLET_PW = os.environ.get("WALLET_PW", "")
 WALLET_NAME = "bagbot"
-# Auto-updated by taonow_sync at 2026-05-23 10:31 UTC
+# Auto-updated by taonow_sync at 2026-05-23 10:34 UTC
 SUBNET_SETTINGS = {
     4: {
-        'buy_lower': 0.02793279,
-        'buy_upper': 0.0,
-        'sell_lower': 0.05307231,
-        'sell_upper': 0.08379838,
-        'max_alpha': 1,
-        'max_tao_per_buy': 0.0,
+        'max_alpha': 30,
+        'max_tao_per_buy': 0.1,
         'max_tao_per_sell': 0.1,
         'max_slippage_percent_per_buy': 1.0,
+        'buy_lower': 0.03347088,
+        'buy_upper': 0.05690049,
+        'sell_lower': 0.07809871,
+        'sell_upper': 0.13946199,
     },
     9: {
-        'buy_lower': 0.01264883,
-        'buy_upper': 0.0,
-        'sell_lower': 0.02403278,
-        'sell_upper': 0.0379465,
-        'max_alpha': 1,
-        'max_tao_per_buy': 0.0,
+        'max_alpha': 30,
+        'max_tao_per_buy': 0.1,
         'max_tao_per_sell': 0.1,
         'max_slippage_percent_per_buy': 1.0,
+        'buy_lower': 0.01530574,
+        'buy_upper': 0.02601975,
+        'sell_lower': 0.03571339,
+        'sell_upper': 0.06377391,
     },
     100: {
         'buy_lower': 0.00649103,
@@ -33,14 +33,14 @@ SUBNET_SETTINGS = {
         'max_tao_per_sell': 0.1,
         'max_slippage_percent_per_buy': 1.0,
     },
-    110: {
-        'buy_lower': 0.00420685,
-        'buy_upper': 0.0,
-        'sell_lower': 0.00799302,
-        'sell_upper': 0.01262056,
-        'max_alpha': 1,
-        'max_tao_per_buy': 0.0,
+    120: {
+        'max_alpha': 30,
+        'max_tao_per_buy': 0.1,
         'max_tao_per_sell': 0.1,
         'max_slippage_percent_per_buy': 1.0,
+        'buy_lower': 0.03775089,
+        'buy_upper': 0.06417651,
+        'sell_lower': 0.08808541,
+        'sell_upper': 0.15729537,
     },
 }
