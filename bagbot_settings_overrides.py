269782d629a11f66bb946f2a220833a5be4eb30d
nhpcorp35 <allenk@jameinc.com>
2026-05-23 06:31:43 -0400

    taonow_sync: auto-update to SN[4, 9, 100, 110]

--- bagbot_settings_overrides.py
+++ bagbot_settings_overrides.py
@@ -1,27 +1,27 @@
 import os
 WALLET_PW = os.environ.get("WALLET_PW", "")
 WALLET_NAME = "bagbot"
-# Auto-updated by taonow_sync at 2026-05-23 10:29 UTC
+# Auto-updated by taonow_sync at 2026-05-23 10:31 UTC
 SUBNET_SETTINGS = {
     4: {
-        'max_alpha': 30,
-        'max_tao_per_buy': 0.1,
+        'buy_lower': 0.02793279,
+        'buy_upper': 0.0,
+        'sell_lower': 0.05307231,
+        'sell_upper': 0.08379838,
+        'max_alpha': 1,
+        'max_tao_per_buy': 0.0,
         'max_tao_per_sell': 0.1,
         'max_slippage_percent_per_buy': 1.0,
-        'buy_lower': 0.03351935,
-        'buy_upper': 0.0569829,
-        'sell_lower': 0.07821182,
-        'sell_upper': 0.13966397,
     },
     9: {
-        'max_alpha': 30,
-        'max_tao_per_buy': 0.1,
+        'buy_lower': 0.01264883,
+        'buy_upper': 0.0,
+        'sell_lower': 0.02403278,
+        'sell_upper': 0.0379465,
+        'max_alpha': 1,
+        'max_tao_per_buy': 0.0,
         'max_tao_per_sell': 0.1,
         'max_slippage_percent_per_buy': 1.0,
-        'buy_lower': 0.0151786,
-        'buy_upper': 0.02580362,
-        'sell_lower': 0.03541673,
-        'sell_upper': 0.06324417,
     },
     100: {
         'buy_lower': 0.00649103,
@@ -34,20 +34,10 @@ SUBNET_SETTINGS = {
         'max_slippage_percent_per_buy': 1.0,
     },
     110: {
-        'max_alpha': 30,
-        'max_tao_per_buy': 0.1,
-        'max_tao_per_sell': 0.1,
-        'max_slippage_percent_per_buy': 1.0,
-        'buy_lower': 0.00504822,
-        'buy_upper': 0.00858198,
-        'sell_lower': 0.01177919,
-        'sell_upper': 0.02103426,
-    },
-    120: {
-        'buy_lower': 0.03119566,
+        'buy_lower': 0.00420685,
         'buy_upper': 0.0,
-        'sell_lower': 0.05927175,
-        'sell_upper': 0.09358697,
+        'sell_lower': 0.00799302,
+        'sell_upper': 0.01262056,
         'max_alpha': 1,
         'max_tao_per_buy': 0.0,
         'max_tao_per_sell': 0.1,
