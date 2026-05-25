3475bcbd135efc3bf89b7c43209e0dac39dd9770
nhpcorp35 <allenk@jameinc.com>
2026-05-24 21:23:53 -0400

    taonow_sync: auto-update to SN[4, 9, 62]

--- bagbot_settings_overrides.py
+++ bagbot_settings_overrides.py
@@ -1,7 +1,7 @@
 import os
 WALLET_PW = os.environ.get("WALLET_PW", "")
 WALLET_NAME = "bagbot"
-# Auto-updated by taonow_sync at 2026-05-25 01:21 UTC
+# Auto-updated by taonow_sync at 2026-05-25 01:23 UTC
 SUBNET_SETTINGS = {
     4: {
         'max_alpha': 30,
