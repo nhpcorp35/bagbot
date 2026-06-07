4fd71dfd45589997f2807660595fe95097d8069f
nhpcorp35 <allenk@jameinc.com>
2026-06-06 23:15:28 -0400

    taonow_sync: auto-update to SN[9, 64, 110]

--- bagbot_settings_overrides.py
+++ bagbot_settings_overrides.py
@@ -1,7 +1,7 @@
 import os
 WALLET_PW = os.environ.get("WALLET_PW", "")
 WALLET_NAME = "bagbot"
-# Auto-updated by taonow_sync at 2026-06-07 02:13 UTC
+# Auto-updated by taonow_sync at 2026-06-07 03:15 UTC
 SUBNET_SETTINGS = {
     9: {
         'buy_lower': 0.02274917,
@@ -25,9 +25,9 @@ SUBNET_SETTINGS = {
         'max_tao_per_buy': 0.1,
         'max_tao_per_sell': 0.1,
         'max_slippage_percent_per_buy': 1.0,
-        'buy_lower': 0.00617372,
-        'buy_upper': 0.00905479,
-        'sell_lower': 0.00987795,
-        'sell_upper': 0.01481693,
+        'buy_lower': 0.00645447,
+        'buy_upper': 0.00946656,
+        'sell_lower': 0.01032716,
+        'sell_upper': 0.01549074,
     },
 }
